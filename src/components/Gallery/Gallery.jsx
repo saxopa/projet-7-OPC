f5df1ab998de08ca94a8fbeec2a7394ee89b97d1
saxopa <alexispontikis1@gmail.com>
2023-10-06 13:38:38 +0200

    Page Gallery jsx

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,19 +1,71 @@
-import datas from '../../data/data'
+import dati from '../../data/datas.json'
 import Card from '../Card/Card'
 
-export default function Gallery() {
+const List = ({ data }) => {
+    const listItems = dati.map((item) => (
+      <div key={item.id}>
+        <Card
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          cover={item.cover}
+        />
+      </div>
+    ));
+  
     return (
-        <main className='gallery'>
-            {datas.map(data => {
-                return (
-                    <Card
-                        key={data.id}
-                        id={data.id}
-                        title={data.title}
-                        cover={data.cover}
-                    />
-                )
-            })}
-        </main>
-    )
-}
+      <div>
+        {listItems}
+      </div>
+    );
+  };
+  
+  export default List;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*export default function JsonReader() {
+    const [jsonData, setJsonData] = useState([]);
+  
+    useEffect(() => {
+      // Fonction asynchrone pour charger le fichier JSON local
+      const fetchJsonData = async () => {
+        try {
+          const response = await fetch('src/data/datas.json'); // Remplacez par le chemin de votre fichier JSON
+          if (!response.ok) {
+            throw new Error('Erreur de chargement du fichier JSON');
+          }
+          const data = await response.json();
+          setJsonData(data);
+        } catch (error) {
+          console.error('Erreur lors du chargement du fichier JSON :', error);
+        }
+      };
+  
+      fetchJsonData(); // Appel de la fonction pour charger le fichier JSON au montage du composant
+    }, []);
+  
+    return (
+      <div>
+      <Card 
+       key={jsonData.id}
+       id={jsonData.id}
+       title={jsonData.title}
+       cover={jsonData.cover}
+      
+      />
+      </div>
+    );
+  }
+  */
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Banner from '../components/Banner/Banner'
 import Gallery from '../components/Gallery/Gallery'
 
+
+
 export default function Home() {
     return (
       <div>
