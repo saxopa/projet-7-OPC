d96e8bf392975cb988dd6e12789b99a3f6aef5f5
saxopa <alexispontikis1@gmail.com>
2023-10-22 19:51:22 +0200

    peaufinage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,33 @@
-import Banner from "../components/Banner/Banner"
-import Collapse from "../components/Collapse/Collapse"
-import "./About.scss"
+import Banner from "../components/Banner/Banner";
+import Collapse from "../components/Collapse/Collapse";
+import "./About.scss";
+import datasAbout from "../data/dataAbout.json";
 
 export default function About() {
-    return (
-        
-        <div>
-            <Banner />
+  return (
+    <div>
+      <Banner />
 
-            <div className="container_collapse_about">
-            <Collapse title="Description" children="Lorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsu" />
-            <Collapse title="Description" children="Lorem ipsu" />
-            <Collapse title="Description" children="Lorem ipsu" />
-            <Collapse title="Description" children="Lorem ipsu" />
-
-            </div>
-
-        
-        </div>
-        
-    )
+      <div className="container_collapse_about">
+        {
+          //créer un composant collapse qui boucle sur un fichier json "datasAbout"
+          //pour afficher les données de la page about
+          //utiliser le composant collapse dans la page about
+          datasAbout.map((data) => {
+            return (
+              <Collapse
+                key={data.id}
+                title={data.title}
+                children={data.children}
+              />
+            );
+          })
+        }
+        <Collapse
+          title="Description"
+          children="Lorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsuLorem ipsu"
+        />
+      </div>
+    </div>
+  );
 }
